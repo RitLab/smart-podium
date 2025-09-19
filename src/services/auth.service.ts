8fc5cacf610e2f5b82dc6cd617be5b7ebc8a8848
Muhammad Abdul Karim <abdkarim.muh@gmail.com>
2025-09-19 21:37:57 +0700

    fix service auth

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -3,23 +3,14 @@ import handler from "./handler";
 
 export const authService = {
   login: async (payload: Login): Promise<Auth> => {
-    return await handler.get<Auth>("/login");
+    return await handler.post<Auth>("/login", payload);
   },
   logout: async (): Promise<any> => {
-    return await handler.get<any>("/logout");
+    return await handler.post<any>("/logout");
   },
   forgotPassword: async (payload: ForgotPassword): Promise<any> => {
-    return await handler.get<any>("/forgot-password", payload);
+    return await handler.post<any>("/forgot-password", payload);
   },
-  //   login: async (payload: Login): Promise<Auth> => {
-  //     return await handler.post<Auth>("/login", payload);
-  //   },
-  //   logout: async (): Promise<any> => {
-  //     return await handler.post<any>("/logout");
-  //   },
-  // forgotPassword: async (payload: ForgotPassword): Promise<any> => {
-  //   return await handler.post<any>("/forgot-password", payload);
-  // },
   getUser: async (): Promise<User> => {
     return await handler.get<User>(`/user`);
   },
