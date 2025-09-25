56cdf23b31a99987b8d225e6509c65db716240aa
Muhammad Abdul Karim <abdkarim.muh@gmail.com>
2025-09-25 21:53:36 +0700

    handling auth services

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -2,15 +2,25 @@ import { Auth, ForgotPassword, Login, User } from "../types/auth.types";
 import handler from "./handler";
 
 export const authService = {
+  // USE MOCK
   login: async (payload: Login): Promise<Auth> => {
-    return await handler.post<Auth>("/login", payload);
+    return await handler.get<Auth>("/login", payload);
   },
   logout: async (): Promise<any> => {
-    return await handler.post<any>("/logout");
+    return await handler.get<any>("/logout");
   },
-  forgotPassword: async (payload: ForgotPassword): Promise<any> => {
-    return await handler.post<any>("/forgot-password", payload);
+  forgotPassword: async (params: ForgotPassword): Promise<any> => {
+    return await handler.get<any>("/forgot-password", params);
   },
+  // login: async (payload: Login): Promise<Auth> => {
+  //   return await handler.post<Auth>("/login", payload);
+  // },
+  // logout: async (): Promise<any> => {
+  //   return await handler.post<any>("/logout");
+  // },
+  // forgotPassword: async (params: ForgotPassword): Promise<any> => {
+  //   return await handler.post<any>("/forgot-password", params);
+  // },
   getUser: async (): Promise<User> => {
     return await handler.get<User>(`/user`);
   },
