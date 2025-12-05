0bbbb75533c699c357d68b9f90156131503c5ee8
Muhammad Abdul Karim <abdkarim.muh@gmail.com>
2025-12-05 18:22:29 +0700

    add workflow github

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,15 +1,36 @@
 import { Class, TokenPayload, TokenResponse, User } from "../types/auth.types";
-import { authURL, handler } from ".";
+// import { authURL, handler } from ".";
 
 export const authService = {
   // USE MOCK
-  getToken: async (payload: TokenPayload): Promise<TokenResponse> => {
-    return await handler.get<TokenResponse>(`${authURL}/get-token`, payload);
+  getToken: async (_: TokenPayload): Promise<TokenResponse> => {
+    // return await handler.get<TokenResponse>(`${authURL}/get-token`, payload);
+    return {
+      token: "123134123123",
+    };
   },
   getCLass: async (): Promise<Class[]> => {
-    return await handler.get<Class[]>(`${authURL}/class`);
+    // return await handler.get<Class[]>(`${authURL}/class`);
+    return [
+      {
+        id: "1",
+        name: "Pelatihan Penanggulangan Terorisme",
+      },
+      {
+        id: "2",
+        name: "Pelatihan Penanggulangan Bencana",
+      },
+    ];
   },
   getUser: async (): Promise<User> => {
-    return await handler.get<User>(`${authURL}/user`);
+    // return await handler.get<User>(`${authURL}/user`);
+    return {
+      id: "1",
+      name: "Bastian Sinaga",
+      email: "test@mail.com",
+      username: "bastian",
+      class: "Pelatihan Penanggulangan Terorisme",
+      photo: "https://i.pravatar.cc/300",
+    };
   },
 };
