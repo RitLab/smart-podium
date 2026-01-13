efdedfba55f41bf1badfd58a35c995871ffc20a4
muchlass19 <muchlas148@gmail.com>
2026-01-13 12:59:03 +0700

    feat: setup auto update configuration

--- electron/preload/index.ts
+++ electron/preload/index.ts
@@ -1,11 +1,44 @@
+// import { ipcRenderer, contextBridge } from 'electron'
+
+// // --------- Expose some API to the Renderer process ---------
+// contextBridge.exposeInMainWorld('ipcRenderer', {
+//   on(...args: Parameters<typeof ipcRenderer.on>) {
+//     const [channel, listener] = args
+//     return ipcRenderer.on(channel, (event, ...args) => listener(event, ...args))
+//   },
+//   off(...args: Parameters<typeof ipcRenderer.off>) {
+//     const [channel, ...omit] = args
+//     return ipcRenderer.off(channel, ...omit)
+//   },
+//   send(...args: Parameters<typeof ipcRenderer.send>) {
+//     const [channel, ...omit] = args
+//     return ipcRenderer.send(channel, ...omit)
+//   },
+//   invoke(...args: Parameters<typeof ipcRenderer.invoke>) {
+//     const [channel, ...omit] = args
+//     return ipcRenderer.invoke(channel, ...omit)
+//   },
+
+//   // You can expose other APTs you need here.
+//   // ...
+// })
+
 import { ipcRenderer, contextBridge } from 'electron'
 
 // --------- Expose some API to the Renderer process ---------
 contextBridge.exposeInMainWorld('ipcRenderer', {
-  on(...args: Parameters<typeof ipcRenderer.on>) {
-    const [channel, listener] = args
-    return ipcRenderer.on(channel, (event, ...args) => listener(event, ...args))
+  // --- UBAH BAGIAN INI (Best Practice untuk React) ---
+  on(channel: string, listener: (event: Electron.IpcRendererEvent, ...args: any[]) => void) {
+    const subscription = (_event: Electron.IpcRendererEvent, ...args: any[]) => listener(_event, ...args)
+    ipcRenderer.on(channel, subscription)
+    
+    // Kembalikan fungsi untuk menghapus listener (cleanup)
+    return () => {
+      ipcRenderer.removeListener(channel, subscription)
+    }
   },
+  // ----------------------------------------------------
+
   off(...args: Parameters<typeof ipcRenderer.off>) {
     const [channel, ...omit] = args
     return ipcRenderer.off(channel, ...omit)
@@ -18,9 +51,6 @@ contextBridge.exposeInMainWorld('ipcRenderer', {
     const [channel, ...omit] = args
     return ipcRenderer.invoke(channel, ...omit)
   },
-
-  // You can expose other APTs you need here.
-  // ...
 })
 
 // --------- Preload scripts loading ---------
