63e89dcd61115c44d581dbff5f4f5800723234a6
muchlass19 <muchlas148@gmail.com>
2026-03-12 14:31:41 +0700

    update ver 30 add frame for minimize

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -43,61 +43,114 @@ let win: BrowserWindow | null = null;
 const preload = path.join(__dirname, "../preload/index.mjs");
 const indexHtml = path.join(RENDERER_DIST, "index.html");
 
+// async function createWindow() {
+//   win = new BrowserWindow({
+//     title: "Main window",
+//     icon: path.join(process.env.VITE_PUBLIC, "favicon.ico"),
+//     fullscreen: true,
+//     webPreferences: {
+//       preload,
+//       webviewTag: true,
+//       // Warning: Enable nodeIntegration and disable contextIsolation is not secure in production
+//       // nodeIntegration: true,
+
+//       // Consider using contextBridge.exposeInMainWorld
+//       // Read more on https://www.electronjs.org/docs/latest/tutorial/context-isolation
+//       // contextIsolation: false,
+//     },
+//   });
+
+//   session.defaultSession.webRequest.onBeforeSendHeaders((details, callback) => {
+//     // Only modify requests to YouTube domains
+//     if (
+//       details.url.includes("youtube.com") ||
+//       details.url.includes("ytimg.com")
+//     ) {
+//       details.requestHeaders["Referer"] = "www.youtube.com"; // A valid YouTube URL
+//     }
+//     callback({ requestHeaders: details.requestHeaders });
+//   });
+
+//   if (VITE_DEV_SERVER_URL) {
+//     // #298
+//     win.loadURL(VITE_DEV_SERVER_URL);
+//     // Open devTool if the app is not packaged
+//     win.webContents.openDevTools();
+//   } else {
+//     win.loadFile(indexHtml);
+//   }
+
+//   // Test actively push message to the Electron-Renderer
+//   win.webContents.on("did-finish-load", () => {
+//     win?.webContents.send("main-process-message", new Date().toLocaleString());
+//   });
+
+//   // Make all links open with the browser, not with the application
+//   win.webContents.setWindowOpenHandler(({ url }) => {
+//     if (url.startsWith("https:")) shell.openExternal(url);
+//     return { action: "deny" };
+//   });
+
+//   // Auto update
+//   update(win);
+// }
+
 async function createWindow() {
   win = new BrowserWindow({
     title: "Main window",
     icon: path.join(process.env.VITE_PUBLIC, "favicon.ico"),
-    fullscreen: true,
+
+    width: 1280,
+    height: 800,
+
+    show: false,
+    frame: true,
+
     webPreferences: {
       preload,
       webviewTag: true,
-      // Warning: Enable nodeIntegration and disable contextIsolation is not secure in production
-      // nodeIntegration: true,
-
-      // Consider using contextBridge.exposeInMainWorld
-      // Read more on https://www.electronjs.org/docs/latest/tutorial/context-isolation
-      // contextIsolation: false,
     },
   });
 
+  win.maximize();
+
+  win.once("ready-to-show", () => {
+    win?.show();
+  });
+
   session.defaultSession.webRequest.onBeforeSendHeaders((details, callback) => {
-    // Only modify requests to YouTube domains
     if (
       details.url.includes("youtube.com") ||
       details.url.includes("ytimg.com")
     ) {
-      details.requestHeaders["Referer"] = "www.youtube.com"; // A valid YouTube URL
+      details.requestHeaders["Referer"] = "https://www.youtube.com/";
     }
+
     callback({ requestHeaders: details.requestHeaders });
   });
 
   if (VITE_DEV_SERVER_URL) {
-    // #298
     win.loadURL(VITE_DEV_SERVER_URL);
-    // Open devTool if the app is not packaged
     win.webContents.openDevTools();
   } else {
     win.loadFile(indexHtml);
   }
 
-  // Test actively push message to the Electron-Renderer
   win.webContents.on("did-finish-load", () => {
     win?.webContents.send("main-process-message", new Date().toLocaleString());
   });
 
-  // Make all links open with the browser, not with the application
   win.webContents.setWindowOpenHandler(({ url }) => {
     if (url.startsWith("https:")) shell.openExternal(url);
     return { action: "deny" };
   });
 
-  // Auto update
   update(win);
 }
 
 app.whenReady().then(() => {
-  Menu.setApplicationMenu(null)
-  createWindow()
+  Menu.setApplicationMenu(null);
+  createWindow();
 });
 
 app.on("window-all-closed", () => {
